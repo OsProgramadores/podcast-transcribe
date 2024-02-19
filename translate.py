d0488915b6fba09997f751cdbbb844db3f9854b8
MatMercer <contact@matbm.net>
2024-02-18 23:25:10 -0300

    Try pt-br model

--- translate.py
+++ translate.py
@@ -1,17 +1,18 @@
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
-# Load model and tokenizer
-model_name = "facebook/mbart-large-50-many-to-many-mmt"
-tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
-model = MBartForConditionalGeneration.from_pretrained(model_name)
+model = M2M100ForConditionalGeneration.from_pretrained("danhsf/m2m100_418M-finetuned-kde4-en-to-pt_BR")
+tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M", src_lang="en", tgt_lang="pt")
 
-# Example: Translate English text to French
-text = "Hello, how are you?"
-# MBart uses language codes, here 'en_XX' for English and 'fr_XX' for French
-tokenizer.src_lang = "en_US"
-encoded_text = tokenizer(text, return_tensors="pt")
-translated_tokens = model.generate(**encoded_text, forced_bos_token_id=tokenizer.lang_code_to_id["pt_XX"])
-translation = tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
+src_text = "Hello, my name is Bob, I like pasta and I'm happy."
+
+model_inputs = tokenizer(src_text, return_tensors="pt")
+
+loss = model(**model_inputs).loss  # forward pass
+
+tokenizer.src_lang = "en"
+encoded_hi = tokenizer(src_text, return_tensors="pt")
+generated_tokens = model.generate(**encoded_hi, forced_bos_token_id=tokenizer.get_lang_id("pt"))
+translation = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
 
 print(translation)
 
